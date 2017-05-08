c927e7547fa1f2385e2de9106cf6f4b32a8a9ec2
Marek Jankech <marek.jankech@outlook.com>
2017-05-08 13:54:56 +0200

    get-libs.sh edit

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget -t 3 http://www.stud.fit.vutbr.cz/~xjanke01/cards.zip
-unzip -o cards.zip
-rm cards.zip
+wget -t 3 http://www.stud.fit.vutbr.cz/~xjanke01/solitaire_data.zip
+unzip -o solitaire_data.zip
+rm solitaire_data.zip
